96b3e176712f145a4b1a9dc5657e6822db2e08ad
Gorka Olalde Mendia <gorka.olalde@gmail.com>
2016-05-04 19:51:44 +0200

    Used IntelliJ code cleanup in the whole project

--- src/main/java/data/Resource.java
+++ src/main/java/data/Resource.java
@@ -1,9 +1,5 @@
 package data;
 
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
-
 import frontend.MainUI;
 import simulation.Simulation;
 import socket.Buzon;
@@ -13,59 +9,71 @@ import socket.MulticastManager;
 import tasks.SenderRunnable;
 import utils.ConfigFile;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 /**
  * Resource
- * 
+ * <p>
  * Resource is the class that manage all the data of the simulation.
  * NOTE: Setters in this class rewrite config file using "rewrite()" method.
- * 
- * @author Skynet Team
  *
+ * @author Skynet Team
  */
 public class Resource extends Thread {
 
-	private MainUI mainUI;
-	private Dispatcher dispatcher;
-	
-	private Buzon<String> simuBuzon;
-	
-	private Simulation simulation;
-	
-	private ScheduledExecutorService scheduler;
-	
-	private MulticastManager multicastManager;
-	
-	private volatile boolean stop = false;
-	
-	public Resource(Dispatcher dispatcher) {
-		this.dispatcher = dispatcher;
-		init();
-		mainUI = new MainUI();
-		mainUI.start();
-		simuBuzon = new Buzon<>(1000);
-		prepareSimulator();
-		initSimulator();
-	}
-	
-	/**
-	 * When the resource thread is running, set's UI variables to be shown to the user.
-	 * Then creates the task running at the background periodically (LOCATION is send each 10 seconds)
-	 * Finnally, start's listening to the mailbox that is storaged the messages.
-	 */
-	@Override
-	public void run() {
+    private MainUI mainUI;
+    private Dispatcher dispatcher;
+
+    private Buzon<String> simuBuzon;
+
+    private Simulation simulation;
+
+    private ScheduledExecutorService scheduler;
+
+    private MulticastManager multicastManager;
+
+    private volatile boolean stop = false;
+
+    public Resource(Dispatcher dispatcher) {
+        this.dispatcher = dispatcher;
+        init();
+        mainUI = new MainUI();
+        mainUI.start();
+        simuBuzon = new Buzon<>(1000);
+        prepareSimulator();
+        initSimulator();
+    }
+
+    /**
+     * When the resource thread is running, set's UI variables to be shown to the user.
+     * Then creates the task running at the background periodically (LOCATION is send each 10 seconds)
+     * Finnally, start's listening to the mailbox that is storaged the messages.
+     */
+    @Override
+    public void run() {
         initThread();
-		while (!stop) {
-			Message msg = dispatcher.receive();
-			switch (msg.getType()) {
-				case "ALERT":       treatAlertMessage(msg);     break;
-				case "ROUTE":       treatRouteMesasge(msg);     break;
-				case "IDASSIGN":    treatIDAssignMessage(msg);  break;
-				case "JOIN":        treatJoinMessage(msg);      break;
-				default:                                        break;
-			}
-		}
-	}
+        while (!stop) {
+            Message msg = dispatcher.receive();
+            switch (msg.getType()) {
+                case "ALERT":
+                    treatAlertMessage(msg);
+                    break;
+                case "ROUTE":
+                    treatRouteMesasge(msg);
+                    break;
+                case "IDASSIGN":
+                    treatIDAssignMessage(msg);
+                    break;
+                case "JOIN":
+                    treatJoinMessage(msg);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
 
     private void initThread() {
         mainUI.setURL(Definitions.socketAddres);
@@ -111,90 +119,91 @@ public class Resource extends Thread {
         multicastManager.start();
     }
 
-	/**
-	 * This method initialize the resource asking for an ID to the server. If the ID is storaged in the configuration file,
-	 * then restores it and set's the resource of the state to 0 if it was waiting or moving to an incident or; if it was
-	 * going back to the base, sends a state 2 to the server to receive the return home route.
-	 */
-	private void init() {
-		if (Definitions.id == -1) {
-			dispatcher.send(Definitions.id, "IDREQUEST", " ");
-			setID(dispatcher.receive("IDASSIGN").getID());
-		} dispatcher.send(Definitions.id, "CONNECTED", " ");
-		if (Definitions.estado == 2) {
-			setEstado(2);
-		} else if (Definitions.estado == 1) {
-			setEstado(0);
-		}
-	}
-	
-	public int getID() {
-		return Definitions.id;
-	}
-	
-	private void setID(int id) {
-		if (mainUI != null) mainUI.setID(id);
-		Definitions.id = id;
-		rewrite();
-	}
-	
-	public int getEstado() {
-		return Definitions.estado;
-	}
+    /**
+     * This method initialize the resource asking for an ID to the server. If the ID is storaged in the configuration file,
+     * then restores it and set's the resource of the state to 0 if it was waiting or moving to an incident or; if it was
+     * going back to the base, sends a state 2 to the server to receive the return home route.
+     */
+    private void init() {
+        if (Definitions.id == -1) {
+            dispatcher.send(Definitions.id, "IDREQUEST", " ");
+            setID(dispatcher.receive("IDASSIGN").getID());
+        }
+        dispatcher.send(Definitions.id, "CONNECTED", " ");
+        if (Definitions.estado == 2) {
+            setEstado(2);
+        } else if (Definitions.estado == 1) {
+            setEstado(0);
+        }
+    }
 
-	public void setEstado(int estado) {
-		if (mainUI != null) mainUI.setEstado(estado);
-		Definitions.estado = estado;
-		dispatcher.send(Definitions.id, "ESTADO", estado);
-		rewrite();
-	}
-	
-	public double getLatitude() {
-		return Definitions.latitude;
-	}
-	
-	public double getLongitude() {
-		return Definitions.longitude;
-	}
-	
-	public String getLocation() {
-		return getLatitude()+","+ getLongitude();
-	}
-	
-	public void setLocation(double latitude, double longitude) {
-		Definitions.latitude = latitude;
-		Definitions.longitude = longitude;
-		mainUI.setLocation(latitude, longitude);
-		rewrite();
-	}
+    public int getID() {
+        return Definitions.id;
+    }
 
-	/**
-	 * This methods creates a periodical runnable method to send specific data to the server periodically.
-	 */
-	private void createTasks() {
+    private void setID(int id) {
+        if (mainUI != null) mainUI.setID(id);
+        Definitions.id = id;
+        rewrite();
+    }
+
+    public int getEstado() {
+        return Definitions.estado;
+    }
+
+    public void setEstado(int estado) {
+        if (mainUI != null) mainUI.setEstado(estado);
+        Definitions.estado = estado;
+        dispatcher.send(Definitions.id, "ESTADO", estado);
+        rewrite();
+    }
+
+    public double getLatitude() {
+        return Definitions.latitude;
+    }
+
+    public double getLongitude() {
+        return Definitions.longitude;
+    }
+
+    public String getLocation() {
+        return getLatitude() + "," + getLongitude();
+    }
+
+    public void setLocation(double latitude, double longitude) {
+        Definitions.latitude = latitude;
+        Definitions.longitude = longitude;
+        mainUI.setLocation(latitude, longitude);
+        rewrite();
+    }
+
+    /**
+     * This methods creates a periodical runnable method to send specific data to the server periodically.
+     */
+    private void createTasks() {
         SenderRunnable scheduledTask = new SenderRunnable(this, dispatcher, "LOCATION");
-		scheduler = Executors.newScheduledThreadPool(1);
-		scheduler.scheduleAtFixedRate(scheduledTask, 0, 10, TimeUnit.SECONDS);
-	}
-	
-	private void prepareSimulator() {
-		simulation = new Simulation(mainUI, simuBuzon, this);
-	}
-	
-	private void initSimulator() {
-		simulation.start();
-	}
-	
-	private void killSimulator() {
-		simulation.kill();
-	}
-	
-	/**
-	 * Rewrite calls "writeAll()" method from "ConfigFile" to write the data with the variables in "Definitions" class.
-	 */
-	private void rewrite() {
-		ConfigFile file = new ConfigFile();
-		file.writeAll();
-	}
-	
+        scheduler = Executors.newScheduledThreadPool(1);
+        scheduler.scheduleAtFixedRate(scheduledTask, 0, 10, TimeUnit.SECONDS);
+    }
+
+    private void prepareSimulator() {
+        simulation = new Simulation(mainUI, simuBuzon, this);
+    }
+
+    private void initSimulator() {
+        simulation.start();
+    }
+
+    private void killSimulator() {
+        simulation.kill();
+    }
+
+    /**
+     * Rewrite calls "writeAll()" method from "ConfigFile" to write the data with the variables in "Definitions" class.
+     */
+    private void rewrite() {
+        ConfigFile file = new ConfigFile();
+        file.writeAll();
+    }
+
 }
